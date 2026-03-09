af13005a03a02f7458ba5716cd6e59dc5ee3aa49
Luiz Fernando <luiz.fernandosv23@gmail.com>
2026-03-08 21:14:17 -0300

    estilização da home

--- view/javascript/GetProdutos.js
+++ view/javascript/GetProdutos.js
@@ -1,36 +1,54 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabelaProdutos = document.getElementById("corpoTabela");
-
-    fetch("../controller/ControllerProdutos.php")
+window.addEventListener('DOMContentLoaded', () => {    
+    // Verifica se ja não há produtos no localstorage
+    const produtosLocal = JSON.parse(localStorage.getItem("produtos"));
+    
+    // caso seja a primeira requisição (localstorage vazio)
+    if(produtosLocal == null){
+        fetch("../controller/ControllerProdutos.php")
         .then(res => res.json())
         .then(response => {
-            console.log(response);
-            console.log(tabelaProdutos);
-            let produtos = response;
-
-            tabelaProdutos.innerHTML = "";
-
-            produtos.forEach(produto => {
-
-                let nomeForm = produto.nome;
-                let imagemForm = produto.imagem;
-                let descricaoForm = produto.descricao;
-                let quantidadeForm = produto.quantidade;
-                let precoForm = produto.preco;
-
-                let linha = `
-            <tr>
-                <td>${nomeForm}</td>
-                <td><img src="${imagemForm}" width="60"></td>
-                <td>${descricaoForm}</td>
-                <td>${quantidadeForm}</td>
-                <td>R$ ${precoForm}</td>
-            </tr>
-        `;
-
-                produtos = response;
-
-                tabelaProdutos.innerHTML += linha;
-            });
+            let produtosBD = response;
+            // salva os produtos no localstorage
+            localStorage.setItem("produtos", JSON.stringify(produtosBD))
+            console.log("requisicao")
+            // renderiza os produtos na tela
+            renderizarProdutos(produtosBD)
         })
+    }
+    else{
+        // usa os produtos do localstorage e renderiza na tela
+        renderizarProdutos(produtosLocal);
+        console.log("localstorage");
+    }
 })
+
+function renderizarProdutos(produtos){
+    const listaProdutos = document.getElementById("listaProdutos");
+    listaProdutos.innerHTML = "";
+
+    // para cada produto no JSON monta um card
+    produtos.forEach(produto => {
+        const cardProduto = `
+       <div class="col-lg-3 col-md-4 col-sm-6 mb-4" data-index="${produto.id}">
+            <div class="card h-100">
+                <img src="${produto.imagem}" class="card-img-top produto-img" alt="Imagem do produto">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <p class="card-title mb-0">${produto.nome}</p>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">${produto.descricao}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between align-items-center">
+                    <span class="fw-bold text-success">
+                        <strong>
+                            R$ ${produto.preco}
+                        </strong>
+                    </span>
+                    <button class="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        </div>
+        `
+        listaProdutos.innerHTML += cardProduto;
+    })
+}
